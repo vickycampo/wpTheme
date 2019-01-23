9dfa1441b195ec47770321c85b2c8fcd9bc21ec0
Vicky Campo <vickycampo@gmail.com>
2019-01-23 15:39:22 +0100

    Create part-navbar-top.php

---
+++ wpTheme/template-parts/header/part-navbar-top.php
@@ -0,0 +1,11 @@
+<?php
+/*
+@package WordPress
+@subpackage wpTheme
+@since wpTheme
+	=====================================
+		TEMPLATE PART: TOP NAVIGATION BAR
+	=====================================
+* Template part in charge of placing and styling the top navigation bar.
+*/
+?>
