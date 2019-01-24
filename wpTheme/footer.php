d8afa5906880ee44ece9b9859956b884354de442
vickycampo <vickycampo@gmail.com>
2019-01-24 22:13:44 +0100

    finish creating the footer parts Next Chapter 07. Bootstrapping WordPress Sidebar & Content Area

--- wpTheme/footer.php
+++ wpTheme/footer.php
@@ -13,11 +13,6 @@ FOOTER
 */
 ?>
 
-<?php
-//get the theme options and defaults
-$ap_options = get_option( 'ap_core_theme_options' );
-$ap_defaults = ap_core_get_theme_defaults();
-?>
 
 </div> <!-- /wrap div-->
 
@@ -25,62 +20,19 @@ $ap_defaults = ap_core_get_theme_defaults();
 <footer class="row">
      <?php tha_footer_top(); ?>
      <?php
-     //ARRAY OF ALL POSSIBLE COMBINATIONS OF SIDEBARD FOR THE FOOTER
-     $id = 'footer-left-box';
-     $footer_sidebar[$id] = 'Footer - Left side box';
-     $id = 'footer-middle-box';
-     $footer_sidebar[$id] = 'Footer - Center box';
-     $id = 'footer-right-box';
-     $footer_sidebar[$id] = 'Footer - Right side box';
-     //we cound how many elements we are using
-     $num_sidebars = count ($footer_sidebar);
-     //we calculate how many cols we need per element
-     $cols = floor ( 12 / $num_sidebars );
-     $cols_class = "col-sm-" . $cols;
+          //add the widget part of the template
+          get_template_part( '\template-parts\footer\part', 'widgets' );
      ?>
-     <!-- CREATE THE ELEMENT IF THE SIDE BAR IS ACTIVE -->
-     <?php
-
-     foreach ($footer_sidebar as $id => $name)
-     {
-
-          if ( is_active_sidebar( $id ) )
-          { ?>
-               <div class="<?php echo ($cols_class);?>" id="<?php echo ( $id );?>">
-                    <ul>
-                         <?php dynamic_sidebar( $id ); ?>
-                    </ul>
-               </div><!-- /<?php echo ( $id );?>-->
-          <?php }
-          else
-          { ?>
-               <div class="<?php echo ($cols_class);?>" id="<?php echo ( $id );?>">
-
-               </div><!-- /<?php echo ( $id );?>-->
-          <?php }
-     }?>
      <div class="spacer-10"></div><!-- /spacer div-->
-     <!-- Add the footer navigation menu-->
-
      <?php
-     echo ('missing widgets / sidebars part');
-     //Top Navitagion bar part
-     get_template_part( '\template-parts\footer\part', 'navbar-footer' )
-     echo ('missing credit part');
-
+          //add the footer navigation bar
+          get_template_part( '\template-parts\footer\part', 'navbar-footer' );
      ?>
-     <!-- Add the credit part of the theme-->
-     <div class="credit">
-          <?php
-          if ( isset( $ap_options['footer'] ) && $ap_options['footer'] != '' )
-          {
-               echo wp_kses_post( stripcslashes( $ap_options['footer'] ) );
-          }
-          else
-          {
-               echo wp_kses_post( $ap_defaults['footer'] );
-          } ?>
-     </div><!-- /credit -->
+     <div class="spacer-10"></div><!-- /spacer div-->
+     <?php
+          //add the footer credits
+          get_template_part( '\template-parts\footer\part', 'credit' );
+     ?>
      <?php tha_footer_bottom(); ?>
 </footer>
 <?php tha_footer_after(); ?>
--- wpTheme/template-parts/footer/part-navbar-footer.php
+++ wpTheme/template-parts/footer/part-navbar-footer.php
@@ -3,9 +3,9 @@
 @package WordPress
 @subpackage wpTheme
 @since wpTheme
-	=====================================
+	============================================
 		TEMPLATE PART: FOOTER NAVIGATION BAR
-	=====================================
+	============================================
 * Template part in charge of placing and styling the footer navigation bar.
 */
 ?>
