6614d906a2a0f24a75277f2ba9223b9a705481d8
vickycampo <vickycampo@gmail.com>
2019-01-26 16:51:59 +0100

    preparing the sidebars

--- wpTheme/index.php
+++ wpTheme/index.php
@@ -18,30 +18,34 @@
      tha_content_before();
      //We determine which side we need to place the content
      //based on which side the sidebar is located at
-     $wpTheme_content = wpTheme_get_content_side(); ?>
+     $wpTheme_content_columns = wpTheme_get_content_columns('body'); ?>
      <!-- add the class to the content div-->
-     <div class="content col-md-9 <?php echo esc_attr( $wpTheme_content ) ?>">
-          <!-- Add hook -->
-          <?php tha_content_top(); ?>
-          <!-- The loop -->
-          <?php if (have_posts()) :
-                    while (have_posts()) : the_post();
-                    //get template part depending on the template format we are displaying
-                    if (locate_template( 'template-parts/post/part', get_post_format()))
-                    {
-                         get_template_part('template-parts/post/part', get_post_format());
-                    }
-                    else
-                    {
-                         get_template_part('template-parts/post/part', 'post');
-                    }
+     <div class="row" >
+          <div class="content <?php echo esc_attr( $wpTheme_content_columns ) ?> order-2 ">
+               <!-- Add hook -->
+               <?php tha_content_top(); ?>
+               <!-- The loop -->
+               <?php if (have_posts()) :
+                         while (have_posts()) : the_post();
+                         //get template part depending on the template format we are displaying
+                         if (locate_template( 'template-parts/post/part', get_post_format()))
+                         {
+                              get_template_part('template-parts/post/part', get_post_format());
+                         }
+                         else
+                         {
+                              get_template_part('template-parts/post/part', 'post');
+                         }
 
-               endwhile; ?>
-               <?php get_template_part( 'parts/part', 'navigation' ); ?>
-          <?php endif; ?>
+                    endwhile; ?>
+                    <?php get_template_part( 'parts/part', 'navigation' ); ?>
+               <?php endif; ?>
 
-          <?php tha_content_bottom(); ?>
-     </div>
-<?php tha_content_after(); ?>
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+               <?php tha_content_bottom(); ?>
+          </div>
+
+          <?php tha_content_after(); ?>
+          <?php get_sidebar('left'); ?>
+          <?php get_sidebar('right'); ?>
+     </div><!--sidebar and content row-->
+     <?php get_footer(); ?>
