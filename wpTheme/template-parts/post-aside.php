de3a2207b0516e29af9180296765a92d2cabf217
vickycampo <vickycampo@gmail.com>
2019-01-29 01:12:31 +0100

    aside post format

---
+++ wpTheme/template-parts/post-aside.php
@@ -0,0 +1,33 @@
+<?php
+/*
+
+@package WordPress
+@subpackage wpTheme
+@since wpTheme
+
+	========================================
+		POST PART FOR A ASIDE POSTTYPE
+	========================================
+* The main template file. It is required in all themes.
+*/
+?>
+<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+
+	<time datetime=<?php the_time('Y-m-d'); ?>></time>
+
+	<?php tha_entry_before(); ?>
+	<section class="entry">
+		<?php tha_entry_top(); ?>
+
+		<?php the_content(__('Read more &raquo;','wpTheme')); ?>
+		<?php get_template_part( 'parts/part', 'link-pages' ); ?>
+
+		<?php tha_entry_bottom(); ?>
+	</section>
+	<?php tha_entry_after(); ?>
+
+	<div class="icon icon-lightbulb pull-left" title="<?php esc_attr_e( 'Aside', 'wpTheme' ); ?>">
+	</div>
+	<?php get_template_part( 'parts/part', 'micropostmeta' ); ?>
+
+</article>
